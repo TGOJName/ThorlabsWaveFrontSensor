13550ff17589cd2f53a3e7823e76e38d6aeda7e4
Oliver Tu <tujuntian@126.com>
2023-03-08 16:12:26 -0500

    Initial commit

---
+++ blacs_tabs.py
@@ -0,0 +1,109 @@
+#####################################################################
+#                                                                   #
+# /labscript_devices/ThorlabsWaveFrontSensor/blacs_tab.py           #
+#                                                                   #
+# Copyright 2021, Philip Starkey; edited by Oliver Tu               #
+#                                                                   #
+# This file is part of labscript_devices, in the labscript suite    #
+# (see http://labscriptsuite.org), and is licensed under the        #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+from blacs.device_base_class import (
+    DeviceTab,
+    define_state,
+    MODE_BUFFERED,
+    MODE_MANUAL,
+    MODE_TRANSITION_TO_BUFFERED,
+    MODE_TRANSITION_TO_MANUAL,
+)
+import labscript_utils.properties
+from qtutils.qt import QtWidgets
+
+class ThorlabsWaveFrontSensorTab(DeviceTab):
+    def initialise_GUI(self):        
+        connection_object = self.settings['connection_table'].find_by_name(self.device_name)
+        connection_table_properties = connection_object.properties
+
+        wfsProp = {}
+        wfsProp['Resolution Index'] = {
+            'base_unit': '',
+            'min': 0,
+            'max': 11,
+            'step': 1,
+            'decimals': 0,
+        }
+        wfsProp['Pupil Center X'] = { # TODO: Determine a reasonable position range
+            'base_unit': 'mm',
+            'min': -4,
+            'max': 4,
+            'step': 0.001,
+            'decimals': 3,
+        }
+        wfsProp['Pupil Center Y'] = {
+            'base_unit': 'mm',
+            'min': -4,
+            'max': 4,
+            'step': 0.001,
+            'decimals': 3,
+        }
+        wfsProp['Pupil Diameter X'] = {
+            'base_unit': 'mm',
+            'min': 0,
+            'max': 4,
+            'step': 0.001,
+            'decimals': 3,
+        }
+        wfsProp['Pupil Diameter Y'] = {
+            'base_unit': 'mm',
+            'min': 0,
+            'max': 4,
+            'step': 0.001,
+            'decimals': 3,
+        }
+        wfsProp['Limited to Pupil?'] = {
+            'base_unit': 'Bool',
+            'min': 0,
+            'max': 1,
+            'step': 1,
+            'decimals': 0,
+        }
+        wfsProp['Highest Zernike Order'] = {
+            'base_unit': '',
+            'min': 2,
+            'max': 10,
+            'step': 1,
+            'decimals': 0,
+        }
+        wfsProp['Fourier Order'] = {
+            'base_unit': '',
+            'min': 2,
+            'max': 6,
+            'step': 2,
+            'decimals': 0,
+        }
+
+        # Create DDS Output objects: we only need frequency for the carrier and frequency and phase for the mod
+        self.create_analog_outputs(wfsProp)
+
+        # Create widgets for outputs defined so far (i.e. analog outputs only)
+        _, AO_widgets, _ = self.auto_create_widgets()  
+        widget_list = [("Analog outputs", AO_widgets)]
+        self.auto_place_widgets(*widget_list)
+        # Connect signals for buttons
+        # Add icons
+        
+        self.sensorIndex = connection_table_properties.get('sensorIndex', None)
+
+        # Create and set the primary worker
+        self.create_worker("main_worker",
+                            'labscript_devices.ThorlabsWaveFrontSensor.blacs_workers.ThorlabsWaveFrontSensorWorker',
+                            {'sensorIndex':self.sensorIndex})
+        
+        self.primary_worker = "main_worker"
+
+        # Set the capabilities of this device
+        self.supports_remote_value_check(False)
+        self.supports_smart_programming(False) 
+
---
+++ labscript_devices.py
@@ -0,0 +1,84 @@
+#####################################################################
+#                                                                   #
+# /labscript_devices/ThorlabsWaveFrontSensor/labscript_devices.py   #
+#                                                                   #
+# Copyright 2021, Philip Starkey; Edited by Oliver Tu               #
+#                                                                   #
+# This file is part of the module labscript_devices, in the         #
+# labscript suite (see http://labscriptsuite.org), and is           #
+# licensed under the Simplified BSD License. See the license.txt    #
+# file in the root of the project for the full license.             #
+#                                                                   #
+#####################################################################
+
+
+from labscript_devices import runviewer_parser
+
+from labscript import config, set_passed_properties,TriggerableDevice
+import labscript_utils.properties
+import numpy as np
+
+__author__ = ['Oliver Tu']
+
+class ThorlabsWaveFrontSensor(TriggerableDevice):
+    """
+    This class is initilzed with the key word argument
+    """
+    description = 'ThorlabsWaveFrontSensor'
+    allowed_children = []
+
+    @set_passed_properties(
+        property_names={
+            'connection_table_properties': [
+                'sensorIndex',
+            ]
+        }
+    )
+    def __init__(
+        self,
+        name,
+        parent_device,
+        sensorIndex,
+        **kwargs
+    ):
+        TriggerableDevice.__init__(self, name, parent_device, **kwargs)
+        self.BLACS_connection = '%u'%(sensorIndex)
+
+    def expose(self, t, trigger_duration = 150e-6):
+        """Request an exposure at the given time. A trigger will be produced by the
+        parent trigger object, with duration trigger_duration, or if not specified, of
+        self.trigger_duration. The frame should have a `name, and optionally a
+        `frametype`, both strings. These determine where the image will be stored in the
+        hdf5 file. `name` should be a description of the image being taken, such as
+        "insitu_absorption" or "fluorescence" or similar. `frametype` is optional and is
+        the type of frame being acquired, for imaging methods that involve multiple
+        frames. For example an absorption image of atoms might have three frames:
+        'probe', 'atoms' and 'background'. For this one might call expose three times
+        with the same name, but three different frametypes.
+        """
+        # Backward compatibility with code that calls expose with name as the first
+        # argument and t as the second argument:
+        # if isinstance(t, str) and isinstance(name, (int, float)):
+        #     msg = """expose() takes `t` as the first argument and `name` as the second
+        #         argument, but was called with a string as the first argument and a
+        #         number as the second. Swapping arguments for compatibility, but you are
+        #         advised to modify your code to the correct argument order."""
+        #     print(dedent(msg), file=sys.stderr)
+        #     t, name = name, t
+        if not trigger_duration > 0:
+            msg = "trigger_duration must be > 0, not %s" % str(trigger_duration)
+            raise ValueError(msg)
+        self.trigger(t, trigger_duration)
+        return trigger_duration
+
+    def generate_code(self, hdf5_file):
+        # dtypes = {'names':['motor1','motor2','motor3','motor4'],'formats':[np.uint32,np.uint32,np.uint32,np.uint32]}
+
+        # out_table = np.zeros(1,dtype=dtypes)# create the table data for the hdf5 file.
+        # out_table['motor1'].fill(self.target_position[0])
+        # out_table['motor2'].fill(self.target_position[1])
+        # out_table['motor3'].fill(self.target_position[2])
+        # out_table['motor4'].fill(self.target_position[3])
+        # grp = self.init_device_group(hdf5_file)
+        # grp.create_dataset('DATA',compression=config.compression,data=out_table) 
+        pass
